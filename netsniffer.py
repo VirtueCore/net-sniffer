cdffb6e7a510a65054fe38e06e348ee92e66ef87
Pioneer876-hack <Orientmanning1@gmail.com>
2025-11-01 13:36:03 -0400

    Latest Update

--- netsniffer.py
+++ netsniffer.py
@@ -1,8 +1,9 @@
 #!/bin/python
 """Capture network packets from NIC"""
+# Coder: Orient Manning (VirtueCore)
 
 # Import module
-from scapy.all import sniff
+from scapy.all import sniff, wrpcap
 
 # Program name
 
@@ -21,36 +22,73 @@ print(NET_SNIFFER)
 def network():
     """Perform network sniffing program code"""
     # Get network interface
-    get_netinf = input("👨🏻‍💻 Enter network interface (i.e. wlan, ethernet): ")
+    interface = input("👨🏻‍💻 Enter network interface (i.e. wlan, ethernet): ")
 
     # Ask use for IP address
     ip_ask = input("🖥️ Enter IP address: ")
 
     try:
-        # Sniff Network Interface and display detail information
-        sniff(iface=get_netinf, prn=lambda x: x.show())
-
         # Sniff IP address and number of packets to capture
-        net_sniff = sniff(ip_ask, count=0, verbose=True)
+        packets = sniff(
+            iface=interface,
+            filter=f"host {ip_ask}",
+            count=0,
+            prn=lambda x: x.show()
+            )
+        return packets
 
     except PermissionError:
         print("🔑 You must run this program as root/administrator.")
+        return None
 
+    except OSError:
+        print("⚠️ Invalid interface or capture error")
+        return None
+
+
+# Function for PCAP file
+def save_pcap(packets):
+    """Saving results to a PCAP file"""
+
+    # No packets
+    if not packets:
+        print("🚫 No packets were captured.")
+
+    # Saved Packets File
+    filename = "packet.pcap"
+
+    # Store or overwrite packets
+    add_packets = input("""
+        Would you like to save packets along with old captured packets?
+        (Yes or No)
+        """).title()
+
+    if add_packets == "Yes":
+        # Don't overwrite
+        wrpcap(filename, packets, append=True)
     else:
-        # Getting packets information
-        print(net_sniff)
+        wrpcap(filename, packets)
+
+    # Information to user
+    print(f"\nYour packets are saved in {filename}.💾")
 
 
-# CTRL+C Function to end program
-def ctrl_c():
+# Main function
+def main():
     """Show an exit message"""
     try:
-        # Call function
-        network()
-    except TypeError:
+        # Assign network function to a variable
+        captured_packets = network()
+
+        # Call PCAP function
+        save_pcap(captured_packets)
+
+    # Keyboard interruption
+    except KeyboardInterrupt:
         # Exit message
         print("\n🤝 Thanks for using Net Sniffer!")
 
 
-# Call CTRL+C Function
-ctrl_c()
+# Call main Function
+if __name__ == '__main__':
+    main()
